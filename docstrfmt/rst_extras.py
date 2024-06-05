338b20bfc0c375ed92a5bf31e3941e08152f6cc4
AugusteBourgois <auguste@forssea-robotics.fr>
2024-06-05 17:45:18 +0200

    add uml

--- docstrfmt/rst_extras.py
+++ docstrfmt/rst_extras.py
@@ -14,6 +14,7 @@ from docutils.parsers.rst import directives, roles
 from docutils.parsers.rst.directives import body, images, misc, parts, tables
 from sphinx.directives import code, other
 from sphinxcontrib.drawio import DrawIOFigure
+from sphinxcontrib.plantuml import UmlDirective
 
 # Import these only to load their domain subclasses.
 from sphinx.domains import c, cpp, python  # noqa: F401
@@ -147,6 +148,7 @@ def register() -> None:
     _add_directive("code-block", code.CodeBlock)
     _add_directive("drawio-figure", DrawIOFigure)
     _add_directive("highlight", code.Highlight)
+    _add_directive("uml", UmlDirective)
 
     for d in set(_subclasses(autodoc.Documenter)):
         if d.objtype != "object":
